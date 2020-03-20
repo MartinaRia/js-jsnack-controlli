4a9ec6fc325d3d32b578277346e5f011187a7907
Martina <martina.ria@hotmail.com>
2020-03-20 15:49:35 +0100

    JS Ex2 complete

--- Ex2/master.js
+++ Ex2/master.js
@@ -2,22 +2,22 @@
 2 - L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 3 - Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. */
 
-/* 1 - L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.*/
+/* 2 - L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
 var result, other
 
-var numUno = parseInt(prompt("Prego inserire un numero"));
+var parolaUno = prompt("Prego inserire una parola");
 
-var numDue = parseInt(prompt("Prego inserire un altro numero"));
+var parolaDue = prompt("Prego inserire un'altra parola");
 
-if (numUno > numDue) {
-  result = numUno;
-  other = numDue;
+if (parolaUno.length < parolaDue.length) {
+  result = parolaUno;
+  other = parolaDue;
 } else {
-  result = numDue;
-  other = numUno;
+  result = parolaDue;
+  other = parolaUno;
 }
 
-document.getElementById('maggiore').innerHTML = result
+document.getElementById('minore').innerHTML = result
 
 document.getElementById('altro').innerHTML = other
