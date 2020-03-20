78480c09f6c78e27ebfa6cfbe964834b857ecd55
Martina <martina.ria@hotmail.com>
2020-03-20 16:00:17 +0100

    JS Ex3 complete

--- Ex3/master.js
+++ Ex3/master.js
@@ -2,22 +2,15 @@
 2 - L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 3 - Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. */
 
-/* 1 - L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.*/
+/* 3 - Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.*/
 
-var result, other
 
 var numUno = parseInt(prompt("Prego inserire un numero"));
-
 var numDue = parseInt(prompt("Prego inserire un altro numero"));
+var numTre = parseInt(prompt("Prego inserire un altro altro numero"));
+var numQuattro = parseInt(prompt("Dai, l'ultimo numero, giuro!"));
+var numCinque = parseInt(prompt("Scherzavo, questo è l'ultimo, davvero davvero."));
 
-if (numUno > numDue) {
-  result = numUno;
-  other = numDue;
-} else {
-  result = numDue;
-  other = numUno;
-}
+var result = numUno + numDue + numTre + numQuattro + numCinque
 
-document.getElementById('maggiore').innerHTML = result
-
-document.getElementById('altro').innerHTML = other
+document.getElementById('somma').innerHTML = result
